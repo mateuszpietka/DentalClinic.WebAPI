f473a8642fded7227537225d58d57f418adbd8e3
Mateusz Pietka <pietka.mate@gmail.com>
2022-12-19 21:25:55 +0100

    Add handle delete employee command

---
+++ Modules/Users/DentalClinic.Users.Application/Command/Handler/DeleteEmployeeCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using DentalClinic.Users.Core.Exceptions;
+using DentalClinic.Users.Core.Repositories;
+using MediatR;
+
+namespace DentalClinic.Users.Application.Command.Handler;
+internal class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand>
+{
+    private readonly IUserRepository _userRepository;
+
+    public DeleteEmployeeCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.EmployeeId);
+
+        if (user == null || (user.Role.Name != "Doctor" && user.Role.Name != "Receptionist"))
+            throw new UserNotFoundException();
+
+        await _userRepository.DeleteAsync(user);
+
+        return Unit.Value;
+    }
+
+}
