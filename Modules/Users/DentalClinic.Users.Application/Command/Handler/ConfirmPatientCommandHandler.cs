09c02943f3f07ee2a9691a3a24a5168869d6a49a
Mateusz Pietka <pietka.mate@gmail.com>
2022-12-19 21:05:48 +0100

    Add handle confirm patient command

---
+++ Modules/Users/DentalClinic.Users.Application/Command/Handler/ConfirmPatientCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using DentalClinic.Users.Core.Exceptions;
+using DentalClinic.Users.Core.Repositories;
+using MediatR;
+
+namespace DentalClinic.Users.Application.Command.Handler;
+internal class ConfirmPatientCommandHandler : IRequestHandler<ConfirmPatientCommand>
+{
+    private readonly IUserRepository _userRepository;
+
+    public ConfirmPatientCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<Unit> Handle(ConfirmPatientCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.PatientId);
+
+        if (user == null)
+            throw new UserNotFoundException();
+
+        user.IsConfirmed = true;
+        await _userRepository.UpdateAsync(user);
+
+        return Unit.Value;
+    }
+
+}
---
+++ Modules/Users/DentalClinic.Users.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using DentalClinic.Shared.Abstarctions.Exceptions;
+using System.Net;
+
+namespace DentalClinic.Users.Core.Exceptions;
+
+public  class UserNotFoundException : CustomException
+{
+    public UserNotFoundException()
+        : base("User not found")
+    {
+
+    }
+
+    public override HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+}
