51d93b69893286fd6b00d62a25ab46ae7491e680
Mateusz Pietka <pietka.mate@gmail.com>
2022-12-20 21:59:33 +0100

    Add extensions to visit schedule api

---
+++ Modules/VisitSchedule/DentalClinic.VisitSchedule.Api/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using DentalClinic.VisitSchedule.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DentalClinic.VisitSchedule.API;
+
+public static class Extensions
+{
+    public static IServiceCollection AddVisitScheduleModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddCoreLayer(configuration);
+        //services.AddApplicationLayer();
+        services.AddInfrastructureLayer(configuration);
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseVisitScheduleModule(this IApplicationBuilder app)
+    {
+        return app;
+    }
+}
