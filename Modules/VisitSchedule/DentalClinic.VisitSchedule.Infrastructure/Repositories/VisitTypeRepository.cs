80529f7356155a93a57b7a8be8ce452bac218031
Mateusz Piętka <mateusz.pietka@aat.pl>
2022-12-21 08:32:12 +0100

    Change TId for VisitTypeRepository

--- Modules/VisitSchedule/DentalClinic.VisitSchedule.Infrastructure/Repositories/VisitTypeRepository.cs
+++ Modules/VisitSchedule/DentalClinic.VisitSchedule.Infrastructure/Repositories/VisitTypeRepository.cs
@@ -5,7 +5,7 @@ using DentalClinic.VisitSchedule.Infrastructure.Context;
 
 namespace DentalClinic.VisitSchedule.Infrastructure.Repositories;
 
-internal class VisitTypeRepository : GenericRepositoryBase<VisitType, long>, IVisitTypeRepository
+internal class VisitTypeRepository : GenericRepositoryBase<VisitType, int>, IVisitTypeRepository
 {
     public VisitTypeRepository(VisitScheduleDbContext context) 
         : base(context)
