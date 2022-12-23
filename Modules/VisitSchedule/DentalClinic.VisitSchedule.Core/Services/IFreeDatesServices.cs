7822482fcdae84645a07318a06b3e0218e5f06f0
Mateusz Piętka <mateusz.pietka@aat.pl>
2022-12-23 08:33:43 +0100

    Add interface free dates services

---
+++ Modules/VisitSchedule/DentalClinic.VisitSchedule.Core/Services/IFreeDatesServices.cs
@@ -0,0 +1,6 @@
+﻿namespace DentalClinic.VisitSchedule.Core.Services;
+
+public interface IFreeDatesServices
+{
+    Task<IEnumerable<DateTime>> GetFreeDates(long doctorId, long visitTypeId, DateTime dateFrom, DateTime dateTo);
+}
