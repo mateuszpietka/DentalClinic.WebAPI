03674491dafef06ad412aa0205cf3b0c08c34712
Mateusz Pietka <pietka.mate@gmail.com>
2022-12-30 14:00:38 +0100

    Add entytie and repo

---
+++ Modules/MedicalRecords/DentalClinic.MedicalRecords.Core/Toothing/Entities/PatientTooth.cs
@@ -0,0 +1,8 @@
+﻿namespace DentalClinic.MedicalRecords.Core.Toothing.Entities;
+public class PatientTooth
+{
+    public long PatientId { get; set; }
+    public int QuadrantCode { get; set; }
+    public int ToothNumber { get; set; }
+    public int Condition { get; set; }
+}
---
+++ Modules/MedicalRecords/DentalClinic.MedicalRecords.Core/Toothing/Repositories/PatientToothRepository.cs
@@ -0,0 +1,8 @@
+﻿using DentalClinic.MedicalRecords.Core.Toothing.Entities;
+using DentalClinic.Shared.Abstarctions.Repostories;
+
+namespace DentalClinic.MedicalRecords.Core.Toothing.Repositories;
+public interface IPatientToothRepository : IGenericRepository<PatientTooth, (long patientId, int quadrantCode, int toothNumber)>
+{
+    Task<IEnumerable<PatientTooth>> GetPatientTeeth(long patientId);
+}
