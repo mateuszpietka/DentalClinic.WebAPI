bdad688ff5fc7f00b013a51af929f55106a5965b
Mateusz Pietka <pietka.mate@gmail.com>
2022-12-30 11:46:14 +0100

    Add patient card dto

---
+++ Modules/MedicalRecords/DentalClinic.MedicalRecords.Application/PatientCars/DTO/PatientCardAnnotationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DentalClinic.MedicalRecords.Application.PatientCars.DTO;
+public class PatientCardAnnotationDto
+{
+    public long Id { get; set; }
+    public string DoctorFullName { get; set; }
+    public DateTime CreationDate { get; set; }
+    public string Contents { get; set; }
+}
---
+++ Modules/MedicalRecords/DentalClinic.MedicalRecords.Application/PatientCars/DTO/PatientCardDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DentalClinic.MedicalRecords.Application.PatientCars.DTO;
+
+public class PatientCardDto
+{
+    public long Id { get; set; }
+    public string PatientFullName { get; set; }
+    public PatientCardAnnotationDto[] PatientCardAnnotations { get; set; }
+}
