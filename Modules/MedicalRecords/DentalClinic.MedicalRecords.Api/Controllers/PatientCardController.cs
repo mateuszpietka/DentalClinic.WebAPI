c3382cd5b176d1896eb1f9c8ece4c8579d31e563
Mateusz Pietka <pietka.mate@gmail.com>
2022-12-30 11:27:41 +0100

    Add patient card controller

---
+++ Modules/MedicalRecords/DentalClinic.MedicalRecords.Api/Controllers/PatientCardController.cs
@@ -0,0 +1,27 @@
+﻿using DentalClinic.MedicalRecords.Application.PatientCars.Command;
+using DentalClinic.MedicalRecords.Application.PatientCars.DTO;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalClinic.MedicalRecords.Api.Controllers;
+[Route("api/patientCard")]
+[ApiController]
+public class PatientCardController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public PatientCardController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("annotation")]
+    [Authorize(Roles = "Doctor")]
+    public async Task<IActionResult> AddPatientCardAnnotation([FromBody] AddPatientCardAnnotationDto addPatientCardAnnotationDto)
+    {
+        await _mediator.Send(new AddPatientCardAnnotationCommand(addPatientCardAnnotationDto));
+
+        return Ok();
+    }
+}
