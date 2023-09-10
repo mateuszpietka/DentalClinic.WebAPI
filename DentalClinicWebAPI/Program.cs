c679fa8cceb0073ca1fcc809338e3c1fa6fb978c
Mateusz Pietka <pietka.mate@gmail.com>
2023-09-10 13:22:44 +0200

    Test

--- DentalClinicWebAPI/Program.cs
+++ DentalClinicWebAPI/Program.cs
@@ -19,7 +19,7 @@ namespace DentalClinicWebAPI
             builder.Services.AddUsersModule(configuration);
             builder.Services.AddVisitScheduleModule(configuration);
             builder.Services.AddMedicalRecordsModule(configuration);
-            builder.Services.AddControllers();
+            builder.Services.AddControllers();//lpasld
             builder.Services.AddSwaggerGen();
             builder.Services.AddCors(options =>
             {
